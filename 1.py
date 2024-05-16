5857a58e031fb14858675934fce135d76172ea2a
Ilkush67 <165833752+Ilkush67@users.noreply.github.com>
2024-05-16 11:34:38 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,21 @@
+# list_ = ['one', 'two', 'three']
+# list_2 = [2, 3, 4, 5, 1]
+# sum_ = 0
+# for i in range(len(list_2)):
+#     sum_ += list_2[i]
+#
+# print(sum_)
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f'{i} * {j} = {i * j}')
+
+car_brands = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in car_brands:
+    print('Я езжу на автомабиле марки ' + i)
+
+cars_count = 0
+for j in range(len(car_brands)):
+    cars_count += 10
+    print(cars_count)
+
+
